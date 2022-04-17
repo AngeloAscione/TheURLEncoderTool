d24bdd2736ddc544e91fd8304ed098398b34bdad
Angelo Ascione <goldrakealtarus@gmail.com>
2022-04-17 17:52:47 +0200

    Update and rename urlEncoder.py to urlTool.py

--- urlTool.py
+++ urlTool.py
@@ -1,7 +1,8 @@
+#Importing section
 from time import sleep
+#Trying to import "termcolor", if not installed the except section will install it
 try:
     from termcolor import colored
-
 except ImportError:
         import os
         ret_code = os.system('pip install termcolor')
@@ -10,7 +11,8 @@ except ImportError:
             exit()
         from termcolor import colored
         print (colored('termcolor has been installed','green'))
-       
+        
+#Defining encode function
 def encode(url, dic):
     encoded_string = ""
     words = url.split(' ')
@@ -23,6 +25,7 @@ def encode(url, dic):
         encoded_string += '%20'
     return encoded_string[:-3]
 
+#Defining decode function
 def decode(url, dic):
     for value in dic.values():
         if value in url:
@@ -30,9 +33,11 @@ def decode(url, dic):
             url = url.replace(value, key)
     return url
 
-
+#Dictionary used for encoding and decoding
 char_dict = {' ': '%20', '!': '%21', '"': '%22', '#': '%23', '$': '%24', '%': '%25', '&': '%26', "'": '%27', '(': '%28', ')': '%29', '*': '%2A', '+': '%2B', ',': '%2C', '-': '%2D', '.': '%2E', '/': '%2F', ':': '%3A', ';': '%3B', '<': '%3C', '=': '%3D', '>': '%3E', '?': '%3F', '@': '%40', '[': '%5B', '\\': '%5C', ']': '%5D', '^': '%5E', '_': '%5F', '`': '%60', '{': '%7B', '|': '%7C', '}': '%7D', '~': '%7E'}
 
+
+#Defining menu function
 def menu():
     print(colored("***************************************", 'blue'))
     print(colored("* Welcome in The URL Encoder Tool     *", 'blue'))
@@ -44,22 +49,24 @@ def menu():
     print(colored("***************************************", 'blue'))
     return int(input(colored("**--> ", 'blue')))
 
+#Defining main function
 def main():
-    scelta = 0
+    choise = 0
     while True:
-        scelta = menu()
+        choise = menu()
         print('')
-        if scelta == 1:
+        
+        if choise == 1:
             print(colored("**Paste the URL to encode**", 'blue'))
             encoded_url = encode(input(colored("**--> ", 'blue')), char_dict)
             print(colored('**Here is your encoded URL --> ', 'blue') + colored(encoded_url, 'green') + colored('**', 'blue'))
             sleep(3)
-        elif scelta == 2:
+        elif choise == 2:
             print(colored("**Paste the URL to decode**", 'blue'))
             decoded_url = decode(input(colored("**--> ", 'blue')), char_dict)
             print(colored('**Here is your decoded URL --> ', 'blue') + colored(decoded_url, 'green') + colored('**', 'blue'))
             sleep(3)
-        elif scelta == 0:
+        elif choise == 0:
             exit()
         else:
             print(colored("**Not Valid**", 'red'))
@@ -67,6 +74,7 @@ def main():
 
         print('')
 
+#Starting of the program
 if __name__ == '__main__':
     print('')
     main()
